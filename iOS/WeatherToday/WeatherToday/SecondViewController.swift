887a0b3ba52a4b6bc622ef79426aa226e46438a2
LeeJoEun-01 <joeun1005@naver.com>
2022-08-28 16:29:39 +0900

    Add SecondViewController TableViewCell

--- iOS/WeatherToday/WeatherToday/SecondViewController.swift
+++ iOS/WeatherToday/WeatherToday/SecondViewController.swift
@@ -7,21 +7,22 @@
 
 import UIKit
 
-class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class SecondViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     //let cellIdentifier: String = "cell"
     let weatherCellIdentifier: String = "weatherCell"
-    var weatherKr: [WeatherKr] = []
+    // 나라 날씨 배열 데이터
+    var kr: [WeatherKr] = []
+    // 날씨 사진 데이터
+    let weatherImgArr = ["sunny","snowy","rainy","cloudy"]
     
     var textToSet: String?
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-            return 1 //섹션 두 개 사용
-    }
     // 테이블뷰에 섹션에 해당하는 로우의 개수
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.weatherKr.count
+        print(self.kr.count)
+        return self.kr.count
     }
     // 해당 로우마다 해당하는 셀을 돌려달라
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -29,34 +30,76 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
         let cell: WeatherTableViewCell =
         tableView.dequeueReusableCell(withIdentifier: self.weatherCellIdentifier, for: indexPath) as! WeatherTableViewCell
            
-        let weather: WeatherKr = self.weatherKr[indexPath.row]
+        let weatherKr: WeatherKr = self.kr[indexPath.row]
         
-        cell.cityLabel?.text = weather.city_name
-        cell.stateLabel?.text = "(\(weather.state))" + "/" + "(\(weather.elsius))"
-        cell.rainfallLabel?.text = "(\(weather.rainfall_probability))"
+        cell.cityLabel?.text = weatherKr.city_name
+        // 배열에 있는 값들이 nil일 수도 있음으로 if let 바인딩일 활용한다.
+        var state: Double = 0.0
+        if let stateDouble = weatherKr.state {
+            if let celsiusDouble = weatherKr.celsius {
+                cell.stateLabel?.text = "섭씨 \(stateDouble)도" + " / " + "화씨 \(celsiusDouble)도"
+                state = stateDouble
+            } else {
+                print("error: SecondViewController1")
+            }
+        } else {
+           print("error: SecondViewController2")
+        }
+        var rain: Int = 0
+        if let rainfallInt = weatherKr.rainfall_probability {
+            cell.rainfallLabel?.text = "강수확률 "+"\(rainfallInt)"+"%"
+            rain = rainfallInt
+        } else {
+           print("error: SecondViewController3")
+        }
+        // 강수량과 섭씨별로 조건에 알맞는 이미지를 넣어준다.
+        if rain >= 70 {
+            let weatherImg = UIImage(named: "\(weatherImgArr[2]).jpg")
+            cell.weatherImage.image = weatherImg
+        } else {
+            if rain >= 35 {
+                if rain <= 50 && state <= 10.0 {
+                    let weatherImg = UIImage(named: "\(weatherImgArr[1]).jpg")
+                    cell.weatherImage.image = weatherImg
+                } else {
+                    let weatherImg = UIImage(named: "\(weatherImgArr[3]).jpg")
+                    cell.weatherImage.image = weatherImg
+                }
+            } else {
+                let weatherImg = UIImage(named: "\(weatherImgArr[0]).jpg")
+                cell.weatherImage.image = weatherImg
+            }
+        }
 
         return cell
     }
+    //tableView cell 높이 조절
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+           return 107
+       }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //self.navigationController?.navigationBar.topItem = "세계 날씨"
-        self.title = self.textToSet
+        //self.title = self.textToSet
         
         let jsonDecoder: JSONDecoder = JSONDecoder()
         //데이터 가져오기
-        guard let dataAsset: NSDataAsset = NSDataAsset(name: "weatherKr") else {
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "kr") else {
             return
         }
         
         do {
-            self.weatherKr = try jsonDecoder.decode([WeatherKr].self, from: dataAsset.data)
+            self.kr = try jsonDecoder.decode([WeatherKr].self, from: dataAsset.data)
+            print("O")
         } catch {
             print(error.localizedDescription)
+            print("X")
         }
         
         self.tableView.reloadData()
+        print(kr)
     }
 
     /*
--- iOS/WeatherToday/WeatherToday/WeatherKr.swift
+++ iOS/WeatherToday/WeatherToday/WeatherKr.swift
@@ -19,10 +19,12 @@ import Foundation
 //JSON 데이터 모양의 타입 만들기
 struct WeatherKr: Codable {
     
-    let city_name: String
-    let state: Int
-    let elsius: Float
-    let rainfall_probability: Int
+    // 변수들을 옵셔널 타입으로 지정해줘야 한다.
+    // 안그러면 "The data couldn’t be read because it is missing." 에러가 나옴...
+    let city_name: String?
+    let state: Double?
+    let celsius: Double?
+    let rainfall_probability: Int?
     
     // 이렇게 데이터를 정리해주면 viewController에서 데이터를 가공할 필요없이 받아서 쓰기만 하면된다.
 //    var nameAndAge: String {
